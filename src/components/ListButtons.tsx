5f6e18752874ba0a7827ea3b373dfcca2ec659ae
ianmsfvenom <iangamer296@gmail.com>
2024-12-16 09:52:15 -0300

    change button style

--- src/components/ListButtons.tsx
+++ src/components/ListButtons.tsx
@@ -1,9 +1,11 @@
+import { title } from "process";
 import styles from "../styles/ListButtons.module.css";
 import Button from "./Button";
 
 const buttons = [
     { title: "VISITE NOSSO SITE", link: "https://ip-sistemas.vercel.app" },
     { title: "SOLICITE UM ORÇAMENTO", link: "https://api.whatsapp.com/send?phone=557187645787&text=Olá%2C%20gostaria%20de%20solicitar%20um%20orçamento." },
+    { title: "VEJA UMA DEMONSTRAÇÃO!", link: "https://demo.filamentphp.com" },
 ];
 
 export default function ListButtons() {
