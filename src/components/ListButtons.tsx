19e027d9ea7606770eb4dc266d7a3abef2b6662a
ianmsfvenom <iangamer296@gmail.com>
2024-12-16 09:53:34 -0300

    change button style

--- src/components/ListButtons.tsx
+++ src/components/ListButtons.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/ListButtons.module.css";
 import Button from "./Button";
 
 const buttons = [
-    { title: "VISITE NOSSO SITE", link: "https://ip-sistemas.vercel.app" },
+    { title: "SAIBA MAIS SOBRE NÓS", link: "https://ip-sistemas.vercel.app" },
     { title: "SOLICITE UM ORÇAMENTO", link: "https://api.whatsapp.com/send?phone=557187645787&text=Olá%2C%20gostaria%20de%20solicitar%20um%20orçamento." },
     { title: "VEJA UMA DEMONSTRAÇÃO!", link: "https://demo.filamentphp.com" },
 ];
